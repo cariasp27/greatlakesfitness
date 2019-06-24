e9a504e67d909b7a5efcbafd8eca8478b154150c
cariasp27 <paul.carias@yahoo.com>
2019-06-24 17:09:26 -0400

    added relations to models

--- models/req.js
+++ models/req.js
@@ -4,7 +4,19 @@ module.exports = function (sequelize, Sequelize) {
 	var Request = sequelize.define('request', {
         time: {type: Sequelize.TEXT},
         date: {type: Sequelize.DATEONLY},
-        accepted: {type: Sequelize.BOOLEAN}
+        accepted: {type: Sequelize.BOOLEAN},
+        t_id: { type: Sequelize.INTEGER,
+                references: {
+                model: Trainer,
+                key: 'id'
+        }
+},
+        c_id: { type: Sequelize.INTEGER,
+                references: {
+                model: Trainer,
+                key: 'id'
+        }
+},
 	});
 
 	return Request;
--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ module.exports = function (sequelize, Sequelize) {
 	var User = sequelize.define('user', {
 		username: { type: Sequelize.TEXT },
 		password: { type: Sequelize.STRING, allowNull: false },
+		
 	});
 
 	return User;
