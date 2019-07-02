e7705d667a50510d2218331a214d24444fa95a37
cariasp27 <paul.carias@yahoo.com>
2019-07-01 21:05:03 -0400

    plz wrk

--- routes/auth.js
+++ routes/auth.js
@@ -127,7 +127,4 @@ module.exports = function (app, passport) {
             res.send({ msg: '\n no user to log out \n' })
         }
     })
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, "../client/build/index.html"));
-    })
 }
