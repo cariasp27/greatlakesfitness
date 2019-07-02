f91bf3a08a4098d93b8bd642ed748e63919258a9
cariasp27 <paul.carias@yahoo.com>
2019-07-01 21:01:21 -0400

    please work

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const morgan   = require('morgan')
 const session  = require('express-session')
 const models   = require('./models')
 const app      = express()
-const PORT     = 8080 || process.env.PORT;
+const PORT     = process.env.PORT || 8080;
 const cors = require('cors')
 
 // MIDDLEWARE
